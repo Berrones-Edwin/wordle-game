d84eed42c847f542be456e4ae1947d206bebba9c
Berrones Edwin <Berrones.Edwin@hotmail.com>
2022-02-11 10:59:23 -0600

    [Feat] component box finish

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,7 +1,24 @@
-const Box = () => {
-  return (
-    <div>Box</div>
-  )
+import classNames from 'classnames/bind'
+import { BoxStatus } from '../types/type'
+import styles from '../styles/box.module.scss'
+
+interface BoxProps {
+  value: string
+  status: BoxStatus
+}
+
+const classes = classNames.bind(styles)
+const Box = ({ value, status }: BoxProps) => {
+
+  const boxStatus = classes({
+    correct: status === 'correct',
+    present: status === 'present',
+    absent: status === 'absent',
+    empty: status === 'empty',
+    edit: status === 'edit'
+  })
+
+  return <div className={boxStatus}>{value}</div>
 }
 
 export default Box
